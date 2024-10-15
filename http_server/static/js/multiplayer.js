18e80cf2b66fa55349259fecb6df09d703e92b6f
mhimavamshi <himavamshi1212@gmail.com>
2024-10-15 21:19:32 +0530

    added multiplayer page

---
+++ http_server/static/js/multiplayer.js
@@ -0,0 +1,15 @@
+let join_button = document.querySelector("#join");
+join_button.onclick = join;
+
+
+let create_button = document.querySelector("#create");
+create_button.onclick = create;
+
+function join() {
+    alert("Joining...");
+}
+
+
+function create() {
+    
+}
