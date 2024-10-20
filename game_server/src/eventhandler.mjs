246486a40dde48582d5159f75efe0ff839886993
mhimavamshi <himavamshi1212@gmail.com>
2024-10-20 18:31:15 +0530

    change structure; add broadcast event

--- game_server/src/eventhandler.mjs
+++ game_server/src/eventhandler.mjs
@@ -1,8 +1,7 @@
 import { EventEmitter } from "events";
-import { Player } from "./game.mjs";
-
-let event_set = new Set(["join"]);
-let event_data_requirements = {"join": ["clientID", "gameID"]}
+import { EVENTS } from "./customevents.mjs";
+import { Game, Player } from "./game.mjs";
+import { event_set, event_data_requirements } from "./eventsinfo.mjs";
 
 function data_is_valid(data) {
     // console.log(`Data ${JSON.stringify(data)} check`);
@@ -23,40 +22,55 @@ function data_is_valid(data) {
 
 const event_emitter = new EventEmitter();
 
-event_emitter.on("join", (data, games, ws)=>{
+event_emitter.on(EVENTS.JOIN, (data, games, ws)=>{
     console.log(games);
     var player;
 
     if(!(data.gameID in games)) {
         console.log(`Creating new game with game ID ${data.gameID}`);
-        games[data.gameID] = [];
+        games[data.gameID] = {"game": new Game(), "players": []};
+        // game = new Game();
+        // games[data.gameID].game = game;
         console.log(games);
     } else {
         console.log(`Room ${data.gameID} already exists`);
-        player = games[data.gameID].find(player => player.clientID == data.clientID);
+        player = games[data.gameID].players.find(player => player.clientID == data.clientID);
         if(player) {
             console.log(`Player ${player.clientID} already in the room ${data.gameID}`);
-            ws.send(JSON.stringify({"event": "rejoined", "data": player.symbol}));
+            player.ws = ws;
+            player.send({"event": EVENTS.REJOINED, "data": {"symbol": player.symbol}});
             return;
         }
     }
 
-    if(games[data.gameID].length < 2) {
+    if(games[data.gameID].players.length < 2) {
         console.log(`Creating player with clientID ${data.clientID} for game ${data.gameID}`);
-        player = new Player(data.clientID);
-        player.symbol = games[data.gameID].length == 0 ? "X": "O";
-        games[data.gameID].push(player);
-        ws.send(JSON.stringify({"event": "joined", "data": player.symbol}));
+        player = new Player(data.clientID, ws);
+        player.symbol = games[data.gameID].players.length == 0 ? "X": "O";
+        games[data.gameID].players.push(player);
+        player.send({"event": EVENTS.JOINED, "data": {"symbol": player.symbol}});
         return;
     }
 
-    ws.send(JSON.stringify({"event": "error", "data": "full room or invalid gameID"}));
+    ws.send(JSON.stringify({"event": EVENTS.ERROR, "data": {"message": "full room or invalid gameID"}}));
 });
 
 
-event_emitter.on("move", (data, games, ws)=>{
+event_emitter.on(EVENTS.MOVE, (data, games, ws)=>{
 
 });
 
+
+event_emitter.on(EVENTS.BROADCAST, (data, games, ws)=>{
+    if(data.gameID in games) {
+        var sender = games[data.gameID].players.find(player => player.clientID == data.clientID);
+        // console.log(`Sender is ${sender}`); 
+        if(sender) {
+            games[data.gameID].players.forEach(player => {
+                player.send({"event": EVENTS.BROADCASTED, "data": {"symbol": sender.symbol, "message": data.message}});
+            });   
+        }
+    }
+});
 
 export { event_emitter, data_is_valid };
