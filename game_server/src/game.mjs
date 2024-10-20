f96814fe62cd4567cc8cf0f97ab39a89dd1dcc5a
mhimavamshi <himavamshi1212@gmail.com>
2024-10-20 16:55:15 +0530

    player object

--- game_server/src/game.mjs
+++ game_server/src/game.mjs
@@ -1,14 +1,10 @@
 class Player {
  
-    constructor(clientID, ws) {
+    constructor(clientID) {
         this.clientID = clientID;
-        this.ws = ws;
         this.symbol = "";
     }
 
-    send(data) {
-        this.ws.send(JSON.stringify(data));
-    }
 }
 
 
