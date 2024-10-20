0b81a21db288178671d0e505e9c6edf5d17e93ed
mhimavamshi <himavamshi1212@gmail.com>
2024-10-20 18:30:23 +0530

    player and game object

--- game_server/src/game.mjs
+++ game_server/src/game.mjs
@@ -1,11 +1,28 @@
 class Player {
  
-    constructor(clientID) {
+    constructor(clientID, ws) {
         this.clientID = clientID;
         this.symbol = "";
+        this.ws = ws;
+    }
+
+    send(data) {
+        // console.log(`Sent ${data} to player ${this.clientID}`);
+        this.ws.send(JSON.stringify(data));
     }
 
 }
 
 
-export { Player };
+class Game {
+
+    constructor() {
+        this.moveNumber = 0;
+        this.moveSymbol = "X";
+
+    }
+
+}
+
+
+export { Game, Player };
