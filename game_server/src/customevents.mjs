c66be72f12c4cc2d3271fefd0927ea8cae797f4c
mhimavamshi <himavamshi1212@gmail.com>
2024-10-20 18:30:48 +0530

    named event constants

---
+++ game_server/src/customevents.mjs
@@ -0,0 +1,13 @@
+const EVENTS = {
+    JOIN: "join",
+    JOINED: "joined",
+    REJOIN: "rejoin",
+    REJOINED: "rejoined",
+    MOVE: "move",
+    ERROR: "error",
+    BROADCAST: "broadcast",
+    BROADCASTED: "broadcasted"
+}
+
+
+export { EVENTS };
