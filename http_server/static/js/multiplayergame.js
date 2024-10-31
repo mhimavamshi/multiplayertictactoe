7549e40959735d56d146817923d7bdab742428e5
mhimavamshi <himavamshi1212@gmail.com>
2024-10-31 18:11:48 +0530

    multiplayer game handling

--- http_server/static/js/multiplayergame.js
+++ http_server/static/js/multiplayergame.js
@@ -1,15 +1,42 @@
 function handle_join(data) {
-    symbolpara.innerHTML = `<b>${data.symbol}</b>`;
+    symbolspan.innerHTML = data.symbol;
+    player_symbol = data.symbol;
+    if(data.full) {
+        gamestatus.innerHTML = `<b>${opposite[data.symbol]} joined<b>`;
+    }
 }
 
 function handle_broadcast(data) {
     messages.innerHTML += `<i><b>${data.symbol}<b>: ${data.message}</i><br>`;
 }
 
+function handle_newplayer(data) {
+    gamestatus.innerHTML = `<b>${data.symbol} joined<b>`;
+    info_store["client_data"] = data;
+}
+
+function handle_rejoin(data) {
+    symbolspan.innerHTML = data.symbol;
+    player_symbol = data.symbol;
+    if(data.full) {
+        gamestatus.innerHTML = `<b>${opposite[data.symbol]} joined<b>`;
+    }
+    make_board(data.state); 
+    show_status(data.last, data.status);
+}
+
+function handle_moved(data) {
+    var [x, y] = data.move.split(" ");
+    game_buttons[x][y].value = data.symbol;
+    show_status(data.symbol, data.status);
+}
+
 let handler_lookup = {
     "joined": handle_join,
-    "rejoined": handle_join,
-    "broadcasted": handle_broadcast
+    "rejoined": handle_rejoin,
+    "broadcasted": handle_broadcast,
+    "newplayer": handle_newplayer,
+    "moved": handle_moved
 }
 
 
