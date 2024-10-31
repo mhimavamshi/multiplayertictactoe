69e750a348b3406eb9086212d7c113cd9c1b0ba0
mhimavamshi <himavamshi1212@gmail.com>
2024-10-31 18:05:56 +0530

    entry point

--- game_server/index.mjs
+++ game_server/index.mjs
@@ -3,7 +3,8 @@ import { event_emitter, data_is_valid } from './src/eventhandler.mjs';
 
 
 const wss = new WebSocketServer({
-  port: 8080,
+  host: "0.0.0.0",
+  port: 4500,
 });
 
 let games = {};
@@ -13,10 +14,9 @@ wss.on('connection', function connection(ws) {
   
     ws.on('message', function message(data) {
         data = JSON.parse(data);
-        console.log(data);
-        if(data_is_valid(data)) {
+        // console.log(data);
+        if(data_is_valid(data))
           event_emitter.emit(data.event.toString(), data.data, games, ws);
-        }
     });
   
 });
